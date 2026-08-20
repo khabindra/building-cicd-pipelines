277ea27d66700ec66533f448fda5ddef3736f6dd
khabindra tamang <pujakhabi78@gmail.com>
2026-08-20 22:52:52 +0545

    fixed the .github/workflows/ci.yml and print_secret.py file

--- print_secret.py
+++ print_secret.py
@@ -1,12 +1,12 @@
 import os
 
 # os.environ.get looks for an environment variable.
-# if it doesn't exist, it returns 'Not Found'
-token = os.environ.get('MY_GITHUB_TOKEN', 'Not Fount')
+# If it doesn't exist, it returns 'Not Found'
+token = os.environ.get("MY_GITHUB_TOKEN", "Not Found")
 
-if token == 'Not Found':
-    print('ERROR: Token not found in environment!')
+if token == "Not Found":
+    print("ERROR: Token not found in environment!")
 else:
-    print('SUCCESS: Token loaded!')
-    # DANGER: We are printing the token to prove a point about masking 
-    print(f'The token is: {token}')
+    print("SUCCESS: Token loaded!")
+    # DANGER: We are printing the token to prove a point about masking
+    print(f"The token is: {token}")
